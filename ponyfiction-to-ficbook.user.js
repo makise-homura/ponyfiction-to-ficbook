674da9ae252a85bbcfcf9be0970e7d9084980147
makise-homura <akemi_homura@kurisa.ch>
2023-06-13 23:13:27 +0300

    Correctly specify class and bg image for link

--- ponyfiction-to-ficbook.user.js
+++ ponyfiction-to-ficbook.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Ponyfiction to Ficbook Export
-// @version     0.3.3
+// @version     0.3.4
 // @description Экспорт открытой страницы понификшена в виде текста для заливки на фикбук
 // @include     http*://ponyfiction.org/story/*
 // @author      makise_homura
@@ -66,6 +66,7 @@ if(document.querySelectorAll('div.chapter-text-block').length > 0)
     link.setAttribute('data-noajax', '1');
     link.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(text));
     link.setAttribute('download', chaptername + '.txt');
-    link.style.background = 'url(https://www.google.com/s2/favicons?domain=ficbook.net)';
+    link.classList.add('scon');
+    link.style.backgroundImage = 'url(https://www.google.com/s2/favicons?domain=ficbook.net)';
     document.querySelector('#story_panel > div > div.icon-group').append(link);
 }
