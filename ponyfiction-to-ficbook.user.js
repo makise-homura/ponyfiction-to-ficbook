ccf6b016156dec39f4db7e137e69a6aa090af519
makise-homura <akemi_homura@kurisa.ch>
2023-06-14 05:34:13 +0300

    Make everything asyncronous (also that improves performance)

--- ponyfiction-to-ficbook.user.js
+++ ponyfiction-to-ficbook.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Ponyfiction to Ficbook Export
-// @version     0.3.5
+// @version     0.4
 // @description Экспорт открытой страницы понификшена в виде текста для заливки на фикбук
 // @include     http*://ponyfiction.org/story/*
 // @author      makise_homura
@@ -8,83 +8,99 @@
 // @grant       none
 // ==/UserScript==
 
-if(document.querySelectorAll('div.chapter-text-block').length > 0)
+document.head.appendChild(document.createElement("style")).innerHTML =
+  '.bg-fb       { background-image: url(https://www.google.com/s2/favicons?domain=ficbook.net); }' +
+  '.bg-load     { opacity: 1; animation: f 1s linear; }' +
+  '@keyframes f { 0%,100% { opacity: 1; } 50% { opacity: 0; } }';
+chaptername = document.title.replace(' — Библиотека ponyfiction.org','');
+var link = document.createElement('a');
+link.setAttribute('title', 'Скачать для фикбука');
+link.setAttribute('data-noajax', '1');
+link.setAttribute('href', '#');
+link.classList.add('scon');
+link.classList.add('bg-fb');
+link.addEventListener("click", () =>
 {
-    // Do it for every chapter on a page, then join the results with astericks block
-    var text = Array.from(document.querySelectorAll('div.chapter-text-block')).map(obj =>
+  link.classList.add('bg-load');
+  setTimeout(() =>
+  {
+    if(document.querySelectorAll('div.chapter-text-block').length > 0)
     {
-      var txt = obj.innerHTML;
-      var span = document.createElement('span');
-      span.innerHTML = txt;
-
-      // Process footnotes
-      span.querySelectorAll('* a.footnote-link').forEach(a =>
+      // Do it for every chapter on a page, then join the results with astericks block
+      var text = Array.from(document.querySelectorAll('div.chapter-text-block')).map(obj =>
       {
-        a.innerHTML = '<footnote>' + span.querySelector('div#'+a.href.replaceAll(/.*#/g,'')).textContent.replaceAll(/.*↑ /g, '') + '</footnote>';
-      });
+        var txt = obj.innerHTML;
+        var span = document.createElement('span');
+        span.innerHTML = txt;
 
-      // Remove headers, chapter comment, fonts, footnote blocks
-      ['h2', 'blockquote', 'font', 'div'].forEach(t =>
-      {
+        // Process footnotes
+        span.querySelectorAll('* a.footnote-link').forEach(a =>
+        {
+          a.innerHTML = '<footnote>' + span.querySelector('div#'+a.href.replaceAll(/.*#/g,'')).textContent.replaceAll(/.*↑ /g, '') + '</footnote>';
+        });
+
+        // Remove headers, chapter comment, fonts, footnote blocks
+        ['h2', 'blockquote', 'font', 'div'].forEach(t =>
+        {
           span.querySelectorAll(t).forEach(p => p.parentNode.removeChild(p));
-      });
+        });
 
-      txt = span.innerHTML;
+        txt = span.innerHTML;
 
-      // Remove newlines
-      txt = txt.replaceAll('\n','');
+        // Remove newlines
+        txt = txt.replaceAll('\n','');
 
-      // Replace <hr> with astericks
-      txt = txt.replaceAll('<hr>','\n<center>* * *</center>\n');
+        // Replace <hr> with astericks
+        txt = txt.replaceAll('<hr>','\n<center>* * *</center>\n');
 
-      // Remove tags enclosing subscript, superscript, links
-      txt = txt.replaceAll('<sup>','').replaceAll('</sup>','');
-      txt = txt.replaceAll('<sub>','').replaceAll('</sub>','');
-      txt = txt.replaceAll(/<a [^>]*>/g,'').replaceAll('</a>','');
+        // Remove tags enclosing subscript, superscript, links
+        txt = txt.replaceAll('<sup>','').replaceAll('</sup>','');
+        txt = txt.replaceAll('<sub>','').replaceAll('</sub>','');
+        txt = txt.replaceAll(/<a [^>]*>/g,'').replaceAll('</a>','');
 
-      // Replace spaces at the start of the line
-      txt = txt.replaceAll(/^\s*/g,'');
+        // Replace spaces at the start of the line
+        txt = txt.replaceAll(/^\s*/g,'');
 
-      // Replace nbsps and other html-entities
-      var ta = document.createElement('textarea');
-      ta.innerHTML = txt;
-      txt = ta.value;
+        // Replace nbsps and other html-entities
+        var ta = document.createElement('textarea');
+        ta.innerHTML = txt;
+        txt = ta.value;
 
-      // Change bold text
-      txt = txt.replaceAll('</strong>','</b>').replaceAll('<strong>','<b>');
+        // Change bold text
+        txt = txt.replaceAll('</strong>','</b>').replaceAll('<strong>','<b>');
 
-      // Change italic text
-      txt = txt.replaceAll('</em>','</i>').replaceAll('<em>','<i>');
+        // Change italic text
+        txt = txt.replaceAll('</em>','</i>').replaceAll('<em>','<i>');
 
-      // Add newlines back at the end of paragraphs and in the line breaks
-      txt = txt.replaceAll('</p>','\n').replaceAll('<br>','\n')
+        // Add newlines back at the end of paragraphs and in the line breaks
+        txt = txt.replaceAll('</p>','\n').replaceAll('<br>','\n')
 
-      // Remove next chapter links
-      txt = txt.replaceAll(/<p class=\"next-chapter-link\">(.*)/g,'');
+        // Remove next chapter links
+        txt = txt.replaceAll(/<p class=\"next-chapter-link\">(.*)/g,'');
 
-      // Change right-aligned paragraphs to right-block
-      txt = txt.replaceAll(/<p align=\"right\">(.*)/g,'<right>$1</right>');
+        // Change right-aligned paragraphs to right-block
+        txt = txt.replaceAll(/<p align=\"right\">(.*)/g,'<right>$1</right>');
 
-      // Change centered paragraphs to center-block
-      txt = txt.replaceAll(/<p align=\"center\">(.*)/g,'<center>$1</center>');
+        // Change centered paragraphs to center-block
+        txt = txt.replaceAll(/<p align=\"center\">(.*)/g,'<center>$1</center>');
 
-      // Change all other paragraphs to tabs
-      txt = txt.replaceAll('<p>','<tab>');
+        // Change all other paragraphs to tabs
+        txt = txt.replaceAll('<p>','<tab>');
 
-      // Replace unnecessary tag reopenings
-      txt = txt.replaceAll('</right>\n<right>','\n').replaceAll('</center>\n<center>','\n').replaceAll('</b>\n<b>','\n').replaceAll('</i>\n<i>','\n');
+        // Replace unnecessary tag reopenings
+        txt = txt.replaceAll('</right>\n<right>','\n').replaceAll('</center>\n<center>','\n').replaceAll('</b>\n<b>','\n').replaceAll('</i>\n<i>','\n');
 
-      return txt;
-    }).join('\n<center>* * *</center>\n');
+        return txt;
+      }).join('\n<center>* * *</center>\n');
 
-    // Create a link and allow download
-    chaptername = document.title.replace(' — Библиотека ponyfiction.org','');
-    var link = document.createElement('a');
-    link.setAttribute('title', 'Скачать для фикбука');
-    link.setAttribute('data-noajax', '1');
-    link.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(text));
-    link.setAttribute('download', chaptername + '.txt');
-    link.classList.add('scon');
-    link.style.backgroundImage = 'url(https://www.google.com/s2/favicons?domain=ficbook.net)';
-    document.querySelector('#story_panel > div > div.icon-group').append(link);
-}
+      // Create a dummy link and download it
+      var dummy = document.createElement('a');
+      dummy.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(text);
+      dummy.target = '#';
+      dummy.download = chaptername + '.txt';
+      dummy.click();
+      link.classList.remove('bg-load');
+    }
+  }, 0);
+});
+document.querySelector('#story_panel > div > div.icon-group').append(link);
